b6ff429f41afe3a530a26c0261193e710fc299eb
Muzi Qiu <qiumuzinaruto@gmail.com>
2023-11-01 12:53:32 +0800

    to-do list example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [todolist, setTodolist] = useState([]);
+  const [completedList, setCompletedList] = useState([]);
+  const handleChange = event => {
+    setInput(event.target.value);
+
+  };
+
+  const handleSubmitNewTodo = () => {
+    setTodolist([...todolist, input]);
+  }
+
+  const handleRemove = (item) => {
+    setTodolist(todolist.filter(a => a !== item));
+    setCompletedList([...completedList, item]);
+  }
+
+  const handleAddBack = (item) => {
+    setCompletedList(completedList.filter(a => a !== item));
+    setTodolist([...todolist, item]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>add to-do<input onChange={handleChange} value={input} /></label>
+      <button onClick={handleSubmitNewTodo}>add</button>
+
+
+      <span></span>
+      <ul id='newList'>
+        {todolist.map((item) => <li><input type="checkbox" onClick={() => handleRemove(item)} /><text>{item}</text></li>)}
+        {completedList.map((item) => <li ><input type="checkbox" onClick={() => handleAddBack(item)} /><text style={{ color: 'grey' }}>{item}</text></li>)}
+
+      </ul>
     </div>
   );
 }
